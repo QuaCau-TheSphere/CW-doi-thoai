b4c349871585daa40c74896c76577e38b52bba81
ooker <ganuongphap@gmail.com>
2024-04-01 15:25:48 +0700

    feat: giao diện lựa chọn xổ xuống

--- Web/islands/KhungKiếmBênTrái.tsx
+++ Web/islands/KhungKiếmBênTrái.tsx
@@ -1,3 +1,4 @@
+import { stringify } from "https://deno.land/std@0.207.0/yaml/mod.ts";
 import { StateUpdater, useState } from "preact/hooks";
 import Fuse from "https://deno.land/x/fuse/dist/fuse.esm.js";
 import { BàiĐăng } from "../../Code%20h%E1%BB%97%20tr%E1%BB%A3/Ki%E1%BB%83u%20cho%20%C4%91%C6%B0%E1%BB%9Dng%20d%E1%BA%ABn,%20vault,%20b%C3%A0i%20%C4%91%C4%83ng,%20d%E1%BB%B1%20%C3%A1n.ts";
@@ -11,7 +12,7 @@ import type {
   TênDanhSách,
 } from "../Code%20h%E1%BB%97%20tr%E1%BB%A3/Ki%E1%BB%83u%20cho%20web.ts";
 
-function SearchList({
+function DanhSáchKếtQuảTìmKiếm({
   listName,
   searchList,
   cursor,
@@ -43,40 +44,44 @@ function SearchList({
   );
 
   function Item({ item }: { item: BàiĐăng | NơiĐăng }) {
+    let dòngChính, dòngPhụ;
     switch (listName) {
       case "bài đăng": {
         item = item as BàiĐăng;
-        let dòngPhụ;
+        dòngChính = <h4 class="h4 tiêu-đề">{item["Tiêu đề"]}</h4>;
+
+        let key, value;
         if (item["Dự án"]["Tên dự án"]) {
-          dòngPhụ = (
-            <span class="dòng-phụ font-xs text-slate-400">
-              <span class="nơi-lưu-bài-đăng">Dự án:</span>{" "}
-              {item["Dự án"]["Tên dự án"]}
-            </span>
-          );
+          key = "Dự án";
+          value = item["Dự án"]["Tên dự án"];
         } else if (item.Vault) {
-          dòngPhụ = (
-            <span class="dòng-phụ font-xs text-slate-400">
-              <span class="nơi-lưu-bài-đăng">Vault:</span> {item.Vault}
-            </span>
-          );
+          key = "Vault";
+          value = item.Vault;
         }
-        return (
+
+        dòngPhụ = (
           <>
-            <h4 class="h4 tiêu-đề">{item["Tiêu đề"]}</h4>
-            {dòngPhụ}
+            <span class="nơi-lưu-bài-đăng">{key}:</span> {value}
           </>
         );
+        break;
       }
       case "nơi đăng":
         item = item as NơiĐăng;
-        return (
-          <>
-            <h4 class="h4">{item["Tên nơi đăng"]}</h4>
-            {item["Tên nền tảng"]}
-          </>
+        dòngChính = <h4 class="h4 tiêu-đề">{item["Tên nơi đăng"]}</h4>;
+        dòngPhụ = (
+          <span class="nơi-đăng">
+            {item["Loại nơi đăng"]} {item["Tên nền tảng"]}{" "}
+            {item["Tên cộng đồng"]}
+          </span>
         );
     }
+    return (
+      <>
+        {dòngChính}
+        <span class="dòng-phụ font-xs text-slate-400">{dòngPhụ}</span>
+      </>
+    );
   }
 }
 
@@ -97,31 +102,9 @@ function SearchDiv(
   const [cursor, setCursor] = useState<Cursor>(0);
   const [selectedItem, setSelectedItem] = useState<MụcĐượcChọn>(undefined);
 
-  function handleKeyDown(e: KeyboardEvent) {
-    if (!searchList) return;
-    if (e.key === "ArrowDown") {
-      const newCursor = Math.min(cursor! + 1, searchList.length - 1);
-      setCursor(newCursor);
-    } else if (e.key === "ArrowUp") {
-      const newCursor = Math.max(0, cursor! - 1);
-      setCursor(newCursor);
-    } else if (e.key === "Enter") {
-      setSelectedItem(searchList[cursor].item);
-      const inputNơiĐăng = document.getElementById("input-nơi-đăng");
-      if (activeList === "bài đăng") {
-        // setSearchInput((selectedItem as BàiĐăng)["Tiêu đề"]);
-        setActiveList("nơi đăng");
-        inputNơiĐăng.focus();
-      } else if (activeList === "nơi đăng") {
-        // setSearchInput((selectedItem as NơiĐăng)["Tên nơi đăng"]);
-        setActiveList(undefined);
-      }
-    }
-  }
   chọnBàiĐăngHoặcNơiĐăng(selectedItem?.item);
-
   const searchListNode = (
-    <SearchList
+    <DanhSáchKếtQuảTìmKiếm
       listName={listName}
       searchList={searchList}
       cursor={cursor}
@@ -153,17 +136,56 @@ function SearchDiv(
       </label>
       <br />
       {activeList === listName ? searchListNode : undefined}
-      Cursor: {cursor}
-      <br />
-      Selected item of list {listName}:{" "}
-      <span id={`Item ${listName}`} class={"result"}>
-        {JSON.stringify(selectedItem)}
-      </span>
+      <KếtQuảĐượcChọn />
       <br />
     </div>
   );
-}
 
+  function handleKeyDown(e: KeyboardEvent) {
+    if (!searchList) return;
+    if (e.key === "ArrowDown") {
+      e.preventDefault();
+      const newCursor = Math.min(cursor! + 1, searchList.length - 1);
+      setCursor(newCursor);
+    } else if (e.key === "ArrowUp") {
+      e.preventDefault();
+      const newCursor = Math.max(0, cursor! - 1);
+      setCursor(newCursor);
+    } else if (e.key === "Enter") {
+      setSelectedItem(searchList[cursor].item);
+      const inputNơiĐăng = document.getElementById("input-nơi-đăng");
+
+      if (activeList === "bài đăng") {
+        // setSearchInput((selectedItem as BàiĐăng)["Tiêu đề"]);
+        setActiveList("nơi đăng");
+        inputNơiĐăng.focus();
+      } else if (activeList === "nơi đăng") {
+        // setSearchInput((selectedItem as NơiĐăng)["Tên nơi đăng"]);
+        setActiveList(undefined);
+      }
+    }
+  }
+  function KếtQuảĐượcChọn() {
+    const lấyGiáTrị = (giáTrị: string | {}) => {
+      if (typeof giáTrị === "object") {
+        return Object.entries(giáTrị).map((j) => <li>{j[0]}: {j[1]}</li>);
+      } else {
+        return giáTrị;
+      }
+    };
+    return (
+      <ul id={`${listName.replace(" ", "-")}-được-chọn`} class={"result"}>
+        {selectedItem
+          ? Object.entries(selectedItem).map((i) => (
+            <li>
+              <span class="font-bold">{i[0]}</span>: {lấyGiáTrị(i[1])}
+            </li>
+          ))
+          : ""}
+      </ul>
+    );
+  }
+}
 export default function KhungKiếmBênTrái(
   { danhSáchNơiĐăng, danhSáchBàiĐăng, chọnBàiĐăng, chọnNơiĐăng }:
     KhungKiếmBênTráiProps,
