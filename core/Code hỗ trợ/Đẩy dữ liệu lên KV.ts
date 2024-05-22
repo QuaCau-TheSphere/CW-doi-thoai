a732176a76872f32896885738fd441cdb5aa88b2
ooker <ganuongphap@gmail.com>
2024-05-22 22:44:35 +0700

    chore: đẩy env lên github

--- core/Code hỗ trợ/Đẩy dữ liệu lên KV.ts
+++ core/Code hỗ trợ/Đẩy dữ liệu lên KV.ts
@@ -1,13 +1,12 @@
 import { walk } from "$std/fs/walk.ts";
-import { basename, extname, SEPARATOR } from "$std/path/mod.ts";
 import { parse } from "$std/yaml/mod.ts";
+import { basename, extname, SEPARATOR } from "$std/path/mod.ts";
 import { tạoKeyKV } from "../../utils/Hàm và kiểu cho API server.ts";
-import { tạoDanhSáchBàiĐăng } from "../B. Tạo kết quả/1. Tạo danh sách tất cả bài đăng/mod.ts";
-import tạoDanhSáchNơiĐăngChưaXácĐịnhVịTrí from "../B. Tạo kết quả/2. Tạo danh sách nơi đăng từ cấu hình/mod.ts";
 import { CấuHìnhChung, NơiĐăngChưaXácĐịnhVịTrí } from "./Hàm và kiểu cho vị trí.tsx";
 import CấuHìnhNơiĐăng, { LoạiCấuHình, VậtThểCấuHìnhNơiĐăng } from "./Kiểu cho nơi đăng.ts";
+import { tạoDanhSáchBàiĐăng } from "../B. Tạo kết quả/1. Tạo danh sách tất cả bài đăng/mod.ts";
+import tạoDanhSáchNơiĐăngChưaXácĐịnhVịTrí from "../B. Tạo kết quả/2. Tạo danh sách nơi đăng từ cấu hình/mod.ts";
 import { kv, THƯ_MỤC_CHỨA_TẤT_CẢ_CÁC_VAULT, THƯ_MỤC_CẤU_HÌNH_NƠI_ĐĂNG, ĐƯỜNG_DẪN_ĐẾN_CẤU_HÌNH_CHUNG, ĐƯỜNG_DẪN_ĐẾN_TẬP_TIN_CSV } from "./env.ts";
-import esthetic from "npm:esthetic";
 
 const danhSáchVậtThểCấuHình: VậtThểCấuHìnhNơiĐăng[] = await tạoDanhSáchCấuHình();
 const cấuHìnhVịTrí = parse(await Deno.readTextFile(ĐƯỜNG_DẪN_ĐẾN_CẤU_HÌNH_CHUNG)) as CấuHìnhChung;
@@ -39,7 +38,7 @@ async function tạoDanhSáchCấuHình() {
 
 async function đẩyBàiĐăngLênKV() {
   const danhSáchBàiĐăng = await tạoDanhSáchBàiĐăng(THƯ_MỤC_CHỨA_TẤT_CẢ_CÁC_VAULT, ĐƯỜNG_DẪN_ĐẾN_TẬP_TIN_CSV, danhSáchVậtThểCấuHình);
-  await Deno.writeTextFile("core/A. Cấu hình/Danh sách tất cả bài đăng.json", esthetic.json(JSON.stringify(danhSáchBàiĐăng)));
+  await Deno.writeTextFile("core/A. Cấu hình/Danh sách tất cả bài đăng.json", JSON.stringify(danhSáchBàiĐăng, null, 2));
   for (const bàiĐăngLấyTừVault of danhSáchBàiĐăng) {
     console.log(bàiĐăngLấyTừVault["Tiêu đề"]);
     const key = tạoKeyKV("bài đăng", bàiĐăngLấyTừVault);
